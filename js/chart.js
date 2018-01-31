64ccb4102c2a10fd78aa0ff8f034403c174d4635
Unknown <superregmail@gmx.de>
2018-02-01 00:29:56 +0100

    code style improvement

--- js/chart.js
+++ js/chart.js
@@ -1,36 +1,29 @@
 nv.addGraph(function() {
-  var chart = nv.models.lineWithFocusChart();
+  var chart = nv.models.lineWithFocusChart()
 
-  chart.xAxis
-    .showMaxMin(false)
-    .tickFormat(function(d) { return d3.time.format('%x %I %p')(new Date(d)) });
+  chart.xAxis.showMaxMin(false).tickFormat(function(d) {
+    return d3.time.format("%x %I %p")(new Date(d))
+  })
 
+  chart.x2Axis.showMaxMin(false).tickFormat(function(d) {
+    return d3.time.format("%x")(new Date(d))
+  })
 
-  chart.x2Axis
-    .showMaxMin(false)
-    .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+  chart.yAxis.axisLabel("Points").tickFormat(d3.format(",r"))
 
+  chart.y2Axis.tickFormat(d3.format(",r"))
 
-  chart.yAxis
-    .axisLabel("Points")
-    .tickFormat(d3.format(',r'));
+  chart.tooltip.contentGenerator(function(data) {})
 
-  chart.y2Axis
-    .tickFormat(d3.format(',r'));
-
-  chart.tooltip.contentGenerator( function(data){
-  } )
-
-  d3.select('#chart svg')
+  d3
+    .select("#chart svg")
     .datum(data())
     .call(chart)
-    ;
 
-  nv.utils.windowResize(chart.update);
+  nv.utils.windowResize(chart.update)
 
-  return chart;
-});
-
+  return chart
+})
 
 function data() {
   var arms_graph = []
@@ -42,35 +35,47 @@ function data() {
   var points_chest = 0
   var points_stomach = 0
   var points_legs = 0
-  for (var i = 0; i < logjson.length; i++){
-        var item = logjson[i]
-        points_arm += parseInt(item.arms)
-        points_chest += parseInt(item.chest)
-        points_stomach += parseInt(item.stomach)
-        points_legs += parseInt(item.legs)
-        timestamp = new Date(parseInt(item.time.year),parseInt(item.time.month)-1,parseInt(item.time.day),parseInt(item.time.hour), parseInt(item.time.minute),0,0)
-        arms_graph.push({x: timestamp, y: points_arm})
-        chest_graph.push({x: timestamp, y: points_chest})
-        stomach_graph.push({x: timestamp, y: points_stomach})
-        legs_graph.push({x: timestamp, y: points_legs})
+  for (var i = 0; i < logjson.length; i++) {
+    var item = logjson[i]
+    points_arm += parseInt(item.arms)
+    points_chest += parseInt(item.chest)
+    points_stomach += parseInt(item.stomach)
+    points_legs += parseInt(item.legs)
+    timestamp = new Date(
+      parseInt(item.time.year),
+      parseInt(item.time.month) - 1,
+      parseInt(item.time.day),
+      parseInt(item.time.hour),
+      parseInt(item.time.minute),
+      0,
+      0
+    )
+    arms_graph.push({ x: timestamp, y: points_arm })
+    chest_graph.push({ x: timestamp, y: points_chest })
+    stomach_graph.push({ x: timestamp, y: points_stomach })
+    legs_graph.push({ x: timestamp, y: points_legs })
   }
 
-  return [{
-      key: 'Arms',
+  return [
+    {
+      key: "Arms",
       values: arms_graph,
-      color: '#f5fdc6'
-    },{
-        key: 'Chest',
-        values: chest_graph,
-        color: '#368f8b'
-    },{
-        key: 'Stomach',
-        values: stomach_graph,
-        color: '#af125a'
-    },{
-        key: 'Legs',
-        values: legs_graph,
-        color: '#ddbea8'
-    }]
-
+      color: "#f5fdc6"
+    },
+    {
+      key: "Chest",
+      values: chest_graph,
+      color: "#368f8b"
+    },
+    {
+      key: "Stomach",
+      values: stomach_graph,
+      color: "#af125a"
+    },
+    {
+      key: "Legs",
+      values: legs_graph,
+      color: "#ddbea8"
+    }
+  ]
 }
--- js/page.js
+++ js/page.js
@@ -1,83 +1,84 @@
 $(window).load(function() {
   $(".btn-nav").on("click tap", function() {
-    $(".nav-container").toggleClass("showNav hideNav").removeClass("hidden");
-    $(".home_content").toggleClass("silder_out");
-    $(".add_content").toggleClass("silder_out");
-    $(".archive_content").toggleClass("silder_out");
-    $(this).toggleClass("animated");
-  });
+    $(".nav-container")
+      .toggleClass("showNav hideNav")
+      .removeClass("hidden")
+    $(".home_content").toggleClass("silder_out")
+    $(".add_content").toggleClass("silder_out")
+    $(".archive_content").toggleClass("silder_out")
+    $(this).toggleClass("animated")
+  })
   $(".home_menue_item").on("click tap", function() {
-    BackEnd.get_active_entries();
-    $(".home_content").removeClass("hidden");
-    $(".add_content").addClass("hidden");
-    $(".archive_content").addClass("hidden");
-    $(this).toggleClass("animated");
-  });
+    BackEnd.get_active_entries()
+    $(".home_content").removeClass("hidden")
+    $(".add_content").addClass("hidden")
+    $(".archive_content").addClass("hidden")
+    $(this).toggleClass("animated")
+  })
   $(".add_menue_item").on("click tap", function() {
-    addoptions();
-    $(".add_content").removeClass("hidden");
-    $(".home_content").addClass("hidden");
-    $(".archive_content").addClass("hidden");
-    $(this).toggleClass("animated");
-  });
+    addoptions()
+    $(".add_content").removeClass("hidden")
+    $(".home_content").addClass("hidden")
+    $(".archive_content").addClass("hidden")
+    $(this).toggleClass("animated")
+  })
   $(".archive_menue_item").on("click tap", function() {
-    BackEnd.get_archive_entries();
-    $(".archive_content").removeClass("hidden");
-    $(".home_content").addClass("hidden");
-    $(".add_content").addClass("hidden");
-    $(this).toggleClass("animated");
-  });
-});
+    BackEnd.get_archive_entries()
+    $(".archive_content").removeClass("hidden")
+    $(".home_content").addClass("hidden")
+    $(".add_content").addClass("hidden")
+    $(this).toggleClass("animated")
+  })
+})
 
-function show_selected(tid){
-  if (tid == -1){
-    document.getElementById('form_input_name').value = "";
-    document.getElementById('form_input_disc').value = "";
-    document.getElementById('form_input_arms').value = 0;
-    document.getElementById('form_input_chest').value = 0;
-    document.getElementById('form_input_stomach').value = 0;
-    document.getElementById('form_input_legs').value = 0;
-    document.getElementById('form_input_tid').value = 0;
-  }else{
-    json_return = JSON.parse(BackEnd.get_entries_by_tid(tid));
-    var item = json_return[0];
-    document.getElementById('form_input_name').value = item.name
-    document.getElementById('form_input_disc').value = item.disc;
-    document.getElementById('form_input_arms').value = item.arms;
-    document.getElementById('form_input_chest').value = item.chest;
-    document.getElementById('form_input_stomach').value = item.stomach;
-    document.getElementById('form_input_legs').value = item.legs;
-    document.getElementById('form_input_tid').value = item.tid;
+function show_selected(tid) {
+  if (tid == -1) {
+    document.getElementById("form_input_name").value = ""
+    document.getElementById("form_input_disc").value = ""
+    document.getElementById("form_input_arms").value = 0
+    document.getElementById("form_input_chest").value = 0
+    document.getElementById("form_input_stomach").value = 0
+    document.getElementById("form_input_legs").value = 0
+    document.getElementById("form_input_tid").value = 0
+  } else {
+    json_return = JSON.parse(BackEnd.get_entries_by_tid(tid))
+    var item = json_return[0]
+    document.getElementById("form_input_name").value = item.name
+    document.getElementById("form_input_disc").value = item.disc
+    document.getElementById("form_input_arms").value = item.arms
+    document.getElementById("form_input_chest").value = item.chest
+    document.getElementById("form_input_stomach").value = item.stomach
+    document.getElementById("form_input_legs").value = item.legs
+    document.getElementById("form_input_tid").value = item.tid
     updateArmsInput(item.arms)
     updateChestInput(item.chest)
     updateStomachInput(item.stomach)
     updateLegsInput(item.legs)
   }
-
 }
 
 function addoptions() {
-  json_return = JSON.parse(BackEnd.get_all_entries());
-  select = document.getElementById('selector');
+  json_return = JSON.parse(BackEnd.get_all_entries())
+  select = document.getElementById("selector")
 
-  for (var i = 0; i < json_return.length; i++){
-      var item = json_return[i];
-      var opt = document.createElement('option');
-      opt.value = item.tid;
-      opt.innerHTML = item.name;
-      select.appendChild(opt);
+  for (var i = 0; i < json_return.length; i++) {
+    var item = json_return[i]
+    var opt = document.createElement("option")
+    opt.value = item.tid
+    opt.innerHTML = item.name
+    select.appendChild(opt)
   }
 }
 
 function updateArmsInput(val) {
-      document.getElementById('form_value_arms').innerHTML=val;
-    }
+  document.getElementById("form_value_arms").innerHTML = val
+}
 function updateChestInput(val) {
-      document.getElementById('form_value_chest').innerHTML=val;
-    }
+  document.getElementById("form_value_chest").innerHTML = val
+}
 function updateStomachInput(val) {
-      document.getElementById('form_value_stomach').innerHTML=val;
-    }
+  document.getElementById("form_value_stomach").innerHTML = val
+}
 function updateLegsInput(val) {
-      document.getElementById('form_value_legs').innerHTML=val;
-    }
+  document.getElementById("form_value_legs").innerHTML = val
+}
